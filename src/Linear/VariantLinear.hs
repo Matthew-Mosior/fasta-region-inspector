017bf0c9080451d198c1457ccc77f81f84977f2d
Matthew-Mosior <mattm.github@gmail.com>
2023-11-24 11:30:58 -0500

    Fixing type definition of variantLinear in src/Linear/VariantLinear.hs.

--- src/Linear/VariantLinear.hs
+++ src/Linear/VariantLinear.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE KindSignatures    #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes        #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module Linear.VariantLinear where
 
@@ -10,14 +15,17 @@ import           Data.Aeson.Types
 import           Data.Text           as DText
 import           Data.Vector.Unboxed as DVU
 import           Effectful
+import           Effectful.Ki
 import           Effectful.Log
 
-variantLinear :: ( MonadIO m
-                 , MonadLog m
+variantLinear :: forall {es :: [Effect]}.
+                 ( StructuredConcurrency :> es
+                 , Log :> es
+                 , IOE :> es
                  )
               => [[(Variant,BioMartRegion,Char)]]
               -> [[[String]]]
-              -> m [[String]]
+              -> Eff es [[String]]
 variantLinear xs
               ys =
   return $ variantsWithinAmbiguityCodesAndTSS xs
